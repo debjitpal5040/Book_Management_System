4a8fabd0c0ada15014e14074f50a26fe97f96adb
Debjit Pal <debjitpal5040@gmail.com>
2023-07-21 00:35:00 +0530

    books

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateBookComponent } from './create-book/create-book.component';
@@ -18,7 +19,9 @@ import { UpdateBookComponent } from './update-book/update-book.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-frontend/src/app/book-details/book-details.component.ts
+++ angular-frontend/src/app/book-details/book-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
+import { ActivatedRoute } from '@angular/router';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.css']
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit {
+
+  id: number
+  book: Book
+  constructor(private route: ActivatedRoute, private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.book = new Book();
+    this.bookService.getBookById(this.id).subscribe(data => {
+      this.book = data;
+    });
+  }
 
 }
