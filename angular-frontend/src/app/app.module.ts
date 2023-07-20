87f730f703747835fa1179cf6c621a7c5cb6742b
Debjit Pal <debjitpal5040@gmail.com>
2023-07-21 00:01:00 +0530

    angular

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateBookComponent } from './create-book/create-book.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { UpdateBookComponent } from './update-book/update-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateBookComponent,
+    BookDetailsComponent,
+    BookListComponent,
+    UpdateBookComponent
   ],
   imports: [
     BrowserModule,
