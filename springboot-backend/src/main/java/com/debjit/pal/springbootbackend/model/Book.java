350c1b5e92bb25349e89b5ff0c03b8cd1c15192d
Debjit Pal <debjitpal5040@gmail.com>
2023-07-20 23:37:02 +0530

    book

---
+++ springboot-backend/src/main/java/com/debjit/pal/springbootbackend/model/Book.java
@@ -0,0 +1,75 @@
+package com.debjit.pal.springbootbackend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "price")
+    private int price;
+
+    public Book() {
+
+    }
+
+    public Book(String name, String author, int price) {
+        super();
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", name=" + name + ", author=" + author + ", price=" + price + "]";
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
