d73cd7b6dd3792379f62605b17d3ada836d2c7a3
Debjit Pal <debjitpal5040@gmail.com>
2023-07-21 21:01:02 +0530

    Update BookRepository.java

--- springboot-backend/src/main/java/com/debjit/pal/springbootbackend/repository/BookRepository.java
+++ springboot-backend/src/main/java/com/debjit/pal/springbootbackend/repository/BookRepository.java
@@ -1,11 +1,9 @@
 package com.debjit.pal.springbootbackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.debjit.pal.springbootbackend.model.Book;
 
-@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
